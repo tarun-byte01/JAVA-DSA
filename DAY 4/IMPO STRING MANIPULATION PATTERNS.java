11f31a5fc639fd2dd7d887af8b9c8d5d9e2d0e4c
Tarun <tarunar006@gmail.com>
2026-09-13 16:26:20 +0530

    Add string manipulation patterns in Java

---
+++ DAY 4/IMPO STRING MANIPULATION PATTERNS.java
@@ -0,0 +1,23 @@
+Reverse
+for(int i = s.length()-1; i >= 0; i--)
+  
+Traverse
+for(int i = 0; i < s.length(); i++)
+  
+Character
+s.charAt(i)
+
+Compare
+s1.equals(s2)
+  
+Length
+s.length()
+  
+Substring
+s.substring(start, end)
+
+Search
+s.contains("abc")
+  
+Replace
+s.replace('a', 'b')
