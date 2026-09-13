e59f8b5b178f5960aee97fe06ef30a7991ed44b2
Tarun <tarunar006@gmail.com>
2026-09-13 16:15:07 +0530

    Add palindrome string checker in Java
    This program checks if a given string is a palindrome by reversing it and comparing it to the original.

---
+++ DAY 4/PALINDROM STRING.java
@@ -0,0 +1,24 @@
+Example:
+
+madam → madam
+  
+class Main {
+    public static void main(String[] args) {
+
+        String s = "madam";
+        String rev = "";
+
+        for(int i = s.length() - 1; i >= 0; i--) {
+            rev += s.charAt(i);
+        }
+
+        if(s.equals(rev))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
+
+Output:
+
+Palindrome
