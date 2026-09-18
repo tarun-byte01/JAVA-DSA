4f7602828eacd95d0031bad31db528e65962e6a6
Tarun <tarunar006@gmail.com>
2026-09-18 09:45:01 +0530

    Add recursive sum function in SUM OF N NUMBERS.java

---
+++ DAY 8/SUM OF N NUMBERS.java
@@ -0,0 +1,13 @@
+class Main {
+    static int sum(int n) {
+
+        if(n == 0)
+            return 0;
+
+        return n + sum(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(5));
+    }
+}
