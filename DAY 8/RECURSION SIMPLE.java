395dd183529347d3cca97384e4e745152b16659c
Tarun <tarunar006@gmail.com>
2026-09-18 09:09:46 +0530

    Add recursive print function in Java

---
+++ DAY 8/RECURSION SIMPLE.java
@@ -0,0 +1,14 @@
+class Main {
+    static void print(int n) {
+
+        if(n == 0)
+            return;
+
+        System.out.println(n);
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+        print(5);
+    }
+}
