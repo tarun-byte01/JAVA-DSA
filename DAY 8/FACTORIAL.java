a826d22359cbb1dd49d22705aa6b540b7cf183b7
Tarun <tarunar006@gmail.com>
2026-09-18 09:34:23 +0530

    Add factorial calculation in FACTORIAL.java

---
+++ DAY 8/FACTORIAL.java
@@ -0,0 +1,13 @@
+class Main {
+    static int factorial(int n) {
+
+        if(n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
