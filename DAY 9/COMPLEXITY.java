ad3b45eeb5115aa3e0368f037d423b89c5432dea
Tarun <tarunar006@gmail.com>
2026-09-19 17:36:15 +0530

    Add complexity analysis for search algorithms

---
+++ DAY 9/COMPLEXITY.java
@@ -0,0 +1,8 @@
+Complexity
+  
+Algorithm                      	Time	                    Space
+Linear Search	                  O(n)                     	O(1)
+Binary Search                 	O(log n)                	O(1)
+First/Last Occurrence	          O(log n)	                O(1)
+Search Insert	                  O(log n)	                O(1)
+Rotated Binary Search	          O(log n)	                O(1)
