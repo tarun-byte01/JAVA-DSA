1360acb127e54715c6edb8b8b0b04ebe3ca5c964
Tarun <tarunar006@gmail.com>
2026-09-20 19:58:42 +0530

    Implement Insertion Sort in Java

---
+++ DAY 10/INSERTION SORT.java
@@ -0,0 +1,22 @@
+class Main {
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 4, 1, 2};
+
+        for (int i = 1; i < arr.length; i++) {
+
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+
+            arr[j + 1] = key;
+        }
+
+        for (int x : arr)
+            System.out.print(x + " ");
+    }
+}
