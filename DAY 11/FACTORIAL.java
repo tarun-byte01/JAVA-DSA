b3325693465559b5b52844660354e27d399d1cf6
Tarun <tarunar006@gmail.com>
2026-09-21 15:08:52 +0530

    Create FACTORIAL.java

---
+++ DAY 11/FACTORIAL.java
@@ -0,0 +1,23 @@
+Formula:
+
+5! = 5 × 4 × 3 × 2 × 1
+   = 120
+Code
+class Main {
+
+    static int factorial(int n) {
+
+        if (n == 0)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(factorial(5));
+    }
+}
+
+Output:
+
+120
