47026d650edac8a13d7fb925ff21e475159d6531
Tarun <tarunar006@gmail.com>
2026-09-24 14:55:25 +0530

    Add Min Heap implementation using PriorityQueue

---
+++ DAY 14/MIN HEAP.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
+        pq.add(5);
+
+        System.out.println(pq.poll());
+    }
+}
