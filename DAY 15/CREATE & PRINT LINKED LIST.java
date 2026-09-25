8262bd6d35cc73a13c9e660b85ca1cd855841ad4
Tarun <tarunar006@gmail.com>
2026-09-25 09:09:34 +0530

    Create and print a linked list in Java
    Implemented a linked list with nodes containing integer data and a method to print the list.

---
+++ DAY 15/CREATE & PRINT LINKED LIST.java
@@ -0,0 +1,25 @@
+class Main {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
